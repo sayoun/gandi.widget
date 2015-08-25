456b157309d10ab4f0a96c09301146fb53bd736d
sayoun <sayoun@gandi.net>
2015-08-25 16:04:27 +0200

    Use Base._add_menuitem and _separator for better readability.

--- gandi/widget/paas.py
+++ gandi/widget/paas.py
@@ -19,86 +19,59 @@ class Paas(Base):
             state = paas['state']
             paas_id = paas['id']
 
-            menu_item = Gtk.ImageMenuItem.new_with_label(name)
-            menu_item.set_always_show_image(True)
-
             if state == "running":
                 img = Gtk.Image.new_from_icon_name(Gtk.STOCK_YES,
                                                    Gtk.IconSize.MENU)
-                menu_item.set_image(img)
             else:
                 img = Gtk.Image.new_from_icon_name(Gtk.STOCK_NO,
                                                    Gtk.IconSize.MENU)
-                menu_item.set_image(img)
 
-            # show the item
-            menu_item.show()
+            menu_item = self._add_menuitem(None, name, img=img)
 
-            # create the submenu for the paas
             sub_menu = Gtk.Menu.new()
             paas = ApiPaas.info(paas_id)
 
-            type = Gtk.MenuItem.new()
-            type.set_label('Type: %s' % paas['type'])
-            type.show()
-            sub_menu.append(type)
+            self._add_menuitem(sub_menu, 'Type: %s' % paas['type'])
+            self._add_menuitem(sub_menu, 'Size: %s' % paas['size'])
+            self._add_menuitem(sub_menu, 'Console: %s' % paas['console'],
+                               action=self.copy,
+                               attr=(paas['console'],))
 
-            size = Gtk.MenuItem.new()
-            size.set_label('Size: %s' % paas['size'])
-            size.show()
-            sub_menu.append(size)
-
-            console = Gtk.MenuItem.new()
-            console.set_label('Console: %s' % paas['console'])
-            console.connect('activate', self.copy, paas['console'])
-            console.show()
-            sub_menu.append(console)
+            self._separator(sub_menu)
 
             vhosts = ApiVhost.list({'paas_id': paas_id})
             for host in vhosts:
                 address = 'http%s://%s' % ('s' if host['cert_id'] else '',
                                            host['name'])
-                item_vhost = Gtk.MenuItem.new()
-                item_vhost.set_label('Vhost: %s' % host['name'])
-                item_vhost.connect('activate', self.open_url, address)
-                item_vhost.show()
-                sub_menu.append(item_vhost)
+                self._add_menuitem(sub_menu, 'Vhost: %s' % host['name'],
+                                   action=self.open_url,
+                                   attr=(address,))
+
+            self._separator(sub_menu)
 
             if state == "running":
-                power_off = Gtk.ImageMenuItem.new_with_label('Stop...')
-                power_off.set_always_show_image(True)
                 img = Gtk.Image.new_from_icon_name("system-shutdown",
                                                    Gtk.IconSize.MENU)
-                power_off.set_image(img)
-                power_off.connect('activate', self.on_power_toggled, name,
-                                  'stop')
-                power_off.show()
-                sub_menu.append(power_off)
+                self._add_menuitem(sub_menu, 'Stop...',
+                                   action=self.on_power_toggled,
+                                   attr=(name, 'stop'),
+                                   img=img)
 
-                reboot = Gtk.ImageMenuItem.new_with_label('Reboot...')
-                reboot.set_always_show_image(True)
                 img = Gtk.Image.new_from_icon_name("system-restart",
                                                    Gtk.IconSize.MENU)
-                reboot.set_image(img)
-                reboot.connect('activate', self.on_power_toggled, name,
-                               'reboot')
-                reboot.show()
-                sub_menu.append(reboot)
-
+                self._add_menuitem(sub_menu, 'Reboot...',
+                                   action=self.on_power_toggled,
+                                   attr=(name, 'reboot'),
+                                   img=img)
             else:
-                power_on = Gtk.ImageMenuItem.new_with_label('Start...')
-                power_on.set_always_show_image(True)
                 img = Gtk.Image.new_from_icon_name("gtk-ok",
                                                    Gtk.IconSize.MENU)
-                power_on.set_image(img)
-                power_on.connect('activate', self.on_power_toggled, name,
-                                 'start')
-                power_on.show()
-                sub_menu.append(power_on)
+                self._add_menuitem(sub_menu, 'Start...',
+                                   action=self.on_power_toggled,
+                                   attr=(name, 'start'),
+                                   img=img)
 
-            seperator = Gtk.SeparatorMenuItem.new()
-            seperator.show()
-            sub_menu.append(seperator)
+            self._separator(sub_menu)
 
             menu_item.set_submenu(sub_menu)
             menu_items.append(menu_item)
@@ -106,19 +79,9 @@ class Paas(Base):
         return menu_items
 
     def on_power_toggled(self, widget, paas_name, action):
-        try:
-            if action == 'start':
-                ApiPaas.start(paas_name, background=True)
-            if action == 'stop':
-                ApiPaas.stop(paas_name, background=True)
-            if action == 'reboot':
-                ApiPaas.reboot(paas_name, background=True)
-        except Exception as err:
-            print('Error: ', err.message)
-            error_indicator = Gtk.ImageMenuItem.new_with_label(
-                'An error occured.')
-            img = Gtk.Image.new_from_icon_name("error", Gtk.IconSize.MENU)
-            error_indicator.set_always_show_image(True)
-            error_indicator.set_image(img)
-            error_indicator.show()
-            self._widget.menu.append(error_indicator)
+        if action == 'start':
+            self._call_api(ApiPaas.start, paas_name, background=True)
+        if action == 'stop':
+            self._call_api(ApiPaas.stop, paas_name, background=True)
+        if action == 'reboot':
+            self._call_api(ApiPaas.reboot, paas_name, background=True)
